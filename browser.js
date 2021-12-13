f6246d448d23604ef15c659e207e3ec75a40513b
nichoth <nichoth@gmail.com>
2021-12-13 10:57:00 -0800

    try using a brower file

---
+++ browser.js
@@ -0,0 +1,26 @@
+import { createRequire } from "module";
+const ref = require('ssb-ref');
+import { visit } from 'unist-util-visit'
+
+const BLOB_REF_LENGTH = `&${Buffer.alloc(32).toString('base64')}.sha256`.length
+
+function cidToUrl (toUrl) {
+    return function () {
+        return (ast) => {
+            visit(ast, 'image', (image) => {
+                if (
+                    image.url &&
+                    typeof image.url === 'string' &&
+                    ref.isBlob(image.url.substr(0, BLOB_REF_LENGTH))
+                ) {
+                    image.url = toUrl(image.url);
+                }
+                return image;
+            });
+            return ast;
+        };
+    }
+}
+
+export default cidToUrl
+
